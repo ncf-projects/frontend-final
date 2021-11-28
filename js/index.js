e86b13b1f52f986c0c5ca6cdcaee320e3ec74837
Zachery Naylor <ender@thefluegges.com>
2021-11-28 15:28:48 -0500

    up

--- js/index.js
+++ js/index.js
@@ -1,15 +1,22 @@
-function createCard(cardTitle, cardSubtitle, cardText) {
+function createCard(className, classCategory, crn, currentDesignation) {
     return (`
     <div class="card ml-1 mr-1">
         <div class="card-body">
-            <h4 class="card-title">${cardTitle}</h4>
-            <h6 class="card-subtitle text-muted">${cardSubtitle}</h6>
+            <h4 class="card-title">${className}</h4>
+            <h5 class="card-subtitle text-muted">${classCategory}</h5>
+            <div class="jumbotron-fluid pt-3 pl-1">
+                <h6 class="card-subtitle underline inline">CRN:</h6>
+                <span>&nbsp;</span>
+                <span class="text-muted">${crn}</span>
+                <br>
+                <h6 class="card-subtitle underline inline">Designation:</h6>
+                <span>&nbsp;</span>
+                <span class="text-muted">${currentDesignation}</span>
+            </div>
         </div>
         <div class="card-body">
-            <p class="card-text">${cardText}</p>
-            <div class="row ml-auto mr-auto">
-                <a href="#" class="card-link">Link 1</a>
-                <a href="#" class="card-link">Link 2</a>
+            <div href="#" class="row ml-auto mr-auto">
+                <a href="#" class="card-link ml-auto mr-auto">View Info</a>
             </div>
         </div>
     </div>
@@ -18,6 +25,6 @@ function createCard(cardTitle, cardSubtitle, cardText) {
 
 window.addEventListener("load", () => {
     const cardRow = document.getElementById("cardRow");
-    cardRow.innerHTML += createCard("Card Title", "Card Subtitle", "Card Text");
-    cardRow.innerHTML += createCard("Card Title 2", "Card Subtitle 2", "Card Text 2");
+    cardRow.innerHTML += createCard("Music Production", "Creative Arts", 634255, "SAT");
+    cardRow.innerHTML += createCard("Roman History", "History", 536612, "SAT");
 })
